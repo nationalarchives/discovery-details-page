ee52352a3561339eb6f275366943e72294d5c90a
Matt Blair <mdrblair@gmail.com>
2019-07-16 11:49:27 +0100

    Adding sticky nav and enhancements to mobile view

---
+++ index_files/extensions.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // When the event DOMContentLoaded occurs, it is safe to access the DOM
+
+    // When the user scrolls the page, execute myFunction
+    window.addEventListener('scroll', myFunctionForSticky);
+
+    // Get the navbar
+    var navbar = document.getElementById("summary");
+
+    // Get the offset position of the navbar
+    var sticky = navbar.offsetHeight + 130;
+
+    // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunctionForSticky() {
+        if (window.pageYOffset > sticky) {
+            console.log(sticky);
+            console.log("window.pageYOffset >= sticky");
+        } else {
+            console.log("Not window.pageYOffset >= sticky");
+        }
+        if (window.pageYOffset > sticky) {
+            navbar.classList.add("sticky")
+        } else {
+            navbar.classList.remove("sticky");
+        }
+    }
+});
