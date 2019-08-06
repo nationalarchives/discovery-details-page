19f66be4045852f6af65a8caa4af17a9d1d4a019
Matt Blair <mdrblair@gmail.com>
2019-08-06 12:50:58 +0100

    Delivery options variants and associated experimental style changes

--- includes/delivery-options.php
+++ includes/delivery-options.php
@@ -33,6 +33,45 @@ if(isset($page)) {
         case 9:
             include($delivery_location . "9" . $file_extension);
             break;
+        case 10:
+            include($delivery_location . "10" . $file_extension);
+            break;
+        case 11:
+            include($delivery_location . "11" . $file_extension);
+            break;
+        case 12:
+            include($delivery_location . "12" . $file_extension);
+            break;
+        case 13:
+            include($delivery_location . "13" . $file_extension);
+            break;
+        case 14:
+            include($delivery_location . "14" . $file_extension);
+            break;
+        case 15:
+            include($delivery_location . "15" . $file_extension);
+            break;
+        case 16:
+            include($delivery_location . "16" . $file_extension);
+            break;
+        case 17:
+            include($delivery_location . "17" . $file_extension);
+            break;
+        case 18:
+            include($delivery_location . "18" . $file_extension);
+            break;
+        case 19:
+            include($delivery_location . "19" . $file_extension);
+            break;
+        case 20:
+            include($delivery_location . "20" . $file_extension);
+            break;
+        case 21:
+            include($delivery_location . "21" . $file_extension);
+            break;
+        case 22:
+            include($delivery_location . "22" . $file_extension);
+            break;
         default:
             include($delivery_location . "1" . $file_extension);
             break;
